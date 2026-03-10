c58636465cdf618bee01c0d1bf90092636aef023
Антон Беляев <avvero@mts.ru>
2026-03-10 12:46:31 +0700

    first commit

--- gun.gd
+++ gun.gd
@@ -7,5 +7,12 @@ func _physics_process(delta: float):
 		look_at(target_enemy.global_position)
 
 func shoot():
-	load("res://bullet.tscn")
-	
+	const BULLET = preload("res://bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
